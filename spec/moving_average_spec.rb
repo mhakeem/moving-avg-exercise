fa9e47a80d8dd7acd53c72c6f984e1b43918c818
Mazin Hakeem <mazhak@msn.com>
2019-12-13 20:03:51 -0700

    added more test specs

--- spec/moving_average_spec.rb
+++ spec/moving_average_spec.rb
@@ -14,5 +14,60 @@ describe MovingAverage do
         _(@moving_average.compute(3, [0, 1, 2, 3])).must_equal [0, 0.5, 1, 2]
       end
     end
+
+    describe 'when given a window size of 5 and an array of [0, 1, -2, 3, -4, 5, -6, 7, -8, 9]' do
+      it 'must return an array of averages that equals to [0, 0.5, -0.3333333333333333, 0.5, -0.4, 0.6, -0.8, 1, -1.2, 1.4]' do
+        _(@moving_average.compute(5, [0, 1, -2, 3, -4, 5, -6, 7, -8, 9])).must_equal [0, 0.5, -0.3333333333333333, 0.5, -0.4, 0.6, -0.8, 1, -1.2, 1.4]
+      end
+    end
+
+    describe 'when given a non-inetger value to window_size' do
+      it 'must throw a TypeError exception' do
+        err = -> {@moving_average.compute('3', [0, 1, 2, 3])}.must_raise TypeError
+        err.message.must_match 'window_size must be an integer'
+      end
+    end
+
+    describe 'when given a non-array argument to values' do
+      it 'must throw a TypeError exception' do
+        err = -> {@moving_average.compute(3, '[0, 1, 2, 3]')}.must_raise TypeError
+        err.message.must_match 'values nust be an array'
+      end
+    end
+    
+    describe 'when given an empty array for values' do
+      it 'must throw a ArgumentError exception' do
+        err = -> {@moving_average.compute(3, [])}.must_raise ArgumentError
+        err.message.must_match 'values cannot be an empty array'
+      end
+    end
+   
+    describe 'when given a window_size smaller than 2' do
+      it 'must throw a ArgumentError exception' do
+        err = -> {@moving_average.compute(1, [0, 1, 2, 3])}.must_raise ArgumentError
+        err.message.must_match 'window_size cannot be less than 2'
+      end
+    end
+
+    describe 'when given a array smaller than 2 to values' do
+      it 'must throw a ArgumentError exception' do
+        err = -> {@moving_average.compute(3, [0])}.must_raise ArgumentError
+        err.message.must_match 'values cannot have less than 2 items'
+      end
+    end
+    
+    describe 'when given a window_size to be greater than the size values array' do
+      it 'must throw a ArgumentError exception' do
+        err = -> {@moving_average.compute(5, [0, 1, 2, 3])}.must_raise ArgumentError
+        err.message.must_match 'window_size cannot be greater than the values array size'
+      end
+    end
+    
+    describe 'when values has a non-numeric element' do
+      it 'must throw a TypeError exception' do
+        err = -> {@moving_average.compute(3, [0, '1', 2, 3])}.must_raise TypeError
+        err.message.must_match 'ound a non-numeric value. values must be an array of floating point numbers'
+      end
+    end
   end
 end
